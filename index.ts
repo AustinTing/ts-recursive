14de7ae38f2f4e554e5641e68165642ddb8d818c
CellBody <austintingwork@gmail.com>
2021-04-21 00:55:42 +0800

    Add pow

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log('TS recursive practice!')
+
+// From https://javascript.info/recursion
+// Power
+type getNum = (x: number, n: number) => number
+const pow: getNum = (x: number, n: number) => {
+  if (n === 0) return 1
+  return x * pow(x, n-1)
+}
+
+console.log(pow(2, 1))
+console.log(pow(2, 2))
+console.log(pow(2, 3))
+
+
